d8f00d211109ad35187fe20996b995eb9bd94952
SeamooseMcG <seamusfilkins@gmail.com>
2026-05-11 15:52:59 -0500

    Add files via upload

--- adventureAssistScripts.js
+++ adventureAssistScripts.js
@@ -1,16 +1,36 @@
+document.onload = titlePage();
 
+function titlePage() {
+    const title = document.getElementById("title");
+    document.getElementById("pagetitle").textContent = title.textContent;
+}
 
+function existingJournalEntries() {
+
+    const entryList = document.getElementById("entrylist");
+
+}
 
 function writeToJournal() {
 
+    let entryID = "entry" + sessionStorage.length;
+
+    sessionStorage.setItem("entryID", entryID);
+    sessionStorage.setItem("entryTitle", document.getElementById("entrytitle").value);
+    sessionStorage.setItem("entryContent", document.getElementById("entrycontent").value);
+
     const newEntry = document.createElement("li");
 
-    const journalEntry = document.getElementById("entrytitle");
-    const journalContent = document.getElementById("entrycontent");
+    const entryTitle = document.createElement("h2");
+    entryTitle.textContent = sessionStorage.getItem("entryTitle").value;
 
-    newEntry.innerHTML = `<h2>${journalEntry}</h2><p>${journalContent}</p>`;
+    const entryContent = document.createElement("p");
+    entryContent.textContent = sessionStorage.getItem("entryContent").value;
 
-    document.getElementById("entrylist").appendChild(newEntry);
+    newEntry.appendChild(entryTitle);
+    newEntry.appendChild(entryContent);
+    document.body.getElementById("entrylist").appendChild(newEntry);
+
 
 
 }
